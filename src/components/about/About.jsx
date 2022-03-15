0fa6fc0a1323b2a0d197f32efe7ef14d47c2e974
hamid-Ft <hamidfattahi.a@gmail.com>
2022-03-16 00:39:38 +0330

    about fin

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,8 +5,8 @@ import { FaAward } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
 import { VscFolderLibrary } from "react-icons/vsc";
 
-const About = () => (
-  <>
+const About = () => {
+  return (
     <section id="about">
       <h5>Get to Know</h5>
       <h2>About Me</h2>
@@ -18,25 +18,27 @@ const About = () => (
           </div>
         </div>
 
-        <div className="about__content"></div>
-        <div className="about__cards">
-          <article className="about__card">
-            <FaAward className="about__icon" />
-            <h5>Experience</h5>
-            <small>10+ Years Working</small>
-          </article>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>10+ Years Working</small>
+            </article>
 
-          <article className="about__card">
-            <FiUsers className="about__icon" />
-            <h5>Clients</h5>
-            <small>10+ clients local</small>
-          </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>10+ clients local</small>
+            </article>
 
-          <article className="about__card">
-            <VscFolderLibrary className="about__icon" />
-            <h5>Experience</h5>
-            <small>100+ completed projects</small>
-          </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>100+ completed projects</small>
+            </article>
+          </div>
+
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae
             magnam corrupti nihil aperiam, eaque repellendus ullam non.
@@ -44,11 +46,13 @@ const About = () => (
             mollitia, ipsa dolorem ipsam.
           </p>
 
-          <a href="contact" className="btn btn-primary"></a>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
-  </>
-);
+  );
+};
 
 export default About;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => (
     <div className="container header__container">
       <h5>Hello I'm</h5>
       <h1>Ehsan Shariffar</h1>
-      <h5 className="text-light">Designer</h5>
+      <h5 className="text-light">2D Artist. Illustrator</h5>
       <CTA />
       <HeaderSocials />
 
