33523d59b6bcd553e24e3b5ba7afc69903cedcca
hamid-Ft <hamidfattahi.a@gmail.com>
2022-03-24 02:43:14 +0430

    update extentionx on vs code

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./About.css";
-import ME from "../../assets/me-about.jpg";
+import ME from "../../assets/ax1.png";
 import { FaAward } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
 import { VscFolderLibrary } from "react-icons/vsc";
